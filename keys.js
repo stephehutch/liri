505ccb9d85b916f44f16386935be490228a32646
stephehutch <42480273+stephehutch@users.noreply.github.com>
2018-12-08 20:54:50 -0800

    deleted api keys
    use your own keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+
 
 exports.spotify = {
-  id: '5c090b5cd8a74da6b53e6ecb9e82badf',
-  secret: 'f9a932fccf2440caa6ee80dd33094c7c'
+  id: x, //No peeking
+  secret: x, //it's a secret
 };
